490efd5844a159985351f9d56d8699138828ce42
soulmachine <soulmachine@gmail.com>
2020-03-06 23:43:31 -0800

    Added a new test case, Huobi USDT hot wallet address

--- tests/balance.spec.ts
+++ tests/balance.spec.ts
@@ -8,7 +8,16 @@ beforeAll(async () => {
 });
 
 test('queryBalance(USDT)', async () => {
-  const balance = await getERC20TokenBalance('0x0681d8Db095565FE8A346fA0277bFfdE9C0eDBBF', 'USDT');
+  // Binance USDT hot wallet address
+  const balanceBinance = await getERC20TokenBalance(
+    '0x0681d8Db095565FE8A346fA0277bFfdE9C0eDBBF',
+    'USDT',
+  );
+  expect(balanceBinance).toBeGreaterThan(0);
 
-  expect(balance).toBeGreaterThan(0);
+  const balanceHuobi = await getERC20TokenBalance(
+    '0x1062a747393198f70f71ec65a582423dba7e5ab3',
+    'USDT',
+  );
+  expect(balanceHuobi).toBeGreaterThan(0);
 });
